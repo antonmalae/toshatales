3a7e4447d1ffa648ab254504934f8616fe8be1a5
Tony kali <aka@example.com>
2025-10-17 21:48:26 +0300

    Global FIX all

--- init-db.sql
+++ init-db.sql
@@ -1,6 +1,15 @@
 -- Инициализация базы данных для Сказок про Крокодила Тоши
 -- Этот файл выполняется при первом запуске PostgreSQL контейнера
 
+-- Создаем стандартного пользователя postgres если его нет
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
+        CREATE ROLE postgres WITH LOGIN SUPERUSER CREATEDB CREATEROLE PASSWORD 'postgres';
+    END IF;
+END
+$$;
+
 -- Создаем расширения если их нет
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
